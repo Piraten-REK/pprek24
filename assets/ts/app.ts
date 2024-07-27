309741873e0fd8213d7425889b648c9c9aefe5a4
Mike Kühnapfel <16109180+veyxos@users.noreply.github.com>
2024-07-27 15:54:45 +0200

    use new class SiteNav

--- assets/ts/app.ts
+++ assets/ts/app.ts
@@ -1,42 +1,46 @@
-import handleOutsideClick from './lib/handleOutsideClick'
-import setUpMenu from './lib/menu'
-import { innerWidth, rem } from './lib/styles'
+// import handleOutsideClick from './lib/handleOutsideClick'
+// import setUpMenu from './lib/menu'
+// import { innerWidth, rem } from './lib/styles'
+import SiteNav from './inc/SiteNav'
 
-const HEADER_PADDING = rem(4)
+// const HEADER_PADDING = rem(4)
 
-const siteNavList = document.querySelector('.site-nav ul') as HTMLUListElement
-const siteNavToggle = document.querySelector('.site-nav-toggle') as HTMLButtonElement
-const siteNav = setUpMenu(siteNavList, siteNavToggle)
+// const siteNavList = document.querySelector('.site-nav ul') as HTMLUListElement
+// const siteNavToggle = document.querySelector('.site-nav-toggle') as HTMLButtonElement
+// const siteNav = setUpMenu(siteNavList, siteNavToggle)
 
-handleOutsideClick(siteNavList, event => {
-  if (!siteNav.status) {
-    return
-  }
+// handleOutsideClick(siteNavList, event => {
+//   if (!siteNav.status) {
+//     return
+//   }
 
-  let node = event.target as Element | null
+//   let node = event.target as Element | null
 
-  while (node != null && node.nodeType !== Node.DOCUMENT_NODE) {
-    if (node === siteNavList || node === siteNavToggle) {
-      return
-    }
-    node = node.parentElement ?? null
-  }
+//   while (node != null && node.nodeType !== Node.DOCUMENT_NODE) {
+//     if (node === siteNavList || node === siteNavToggle) {
+//       return
+//     }
+//     node = node.parentElement ?? null
+//   }
 
-  siteNav.close()
-})
+//   siteNav.close()
+// })
 
-const siteNavNav = document.querySelector('.site-nav') as HTMLAreaElement
-const navWidth = siteNavNav.clientWidth
+const siteNav = new SiteNav()
+console.log(siteNav)
 
-function siteNavWatcher (): (() => void) {
-  const headerElement = document.querySelector('.site-header') as HTMLAreaElement
-  const headerWidth = innerWidth(headerElement)
-  const titleElement = document.querySelector('.site-title') as HTMLDivElement
-  const titleWidth = titleElement.clientWidth
-  const delta = headerWidth - titleWidth - HEADER_PADDING
+// const siteNavNav = document.querySelector('.site-nav') as HTMLAreaElement
+// const navWidth = siteNavNav.clientWidth
 
-  document.body.setAttribute('data-mobile-nav', (navWidth > delta).toString())
+// function siteNavWatcher (): (() => void) {
+//   const headerElement = document.querySelector('.site-header') as HTMLAreaElement
+//   const headerWidth = innerWidth(headerElement)
+//   const titleElement = document.querySelector('.site-title') as HTMLDivElement
+//   const titleWidth = titleElement.clientWidth
+//   const delta = headerWidth - titleWidth - HEADER_PADDING
 
-  return siteNavWatcher
-}
-window.addEventListener('resize', siteNavWatcher())
+//   document.body.setAttribute('data-mobile-nav', (navWidth > delta).toString())
+
+//   return siteNavWatcher
+// }
+// window.addEventListener('resize', siteNavWatcher())
